45fe9134ca19b9bae90c2113b098f0060de705cc
Dm Dv <dimos-d@yandex.ru>
2012-04-17 00:14:09 +0400

    First commit

---
+++ src/newpythonproject.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Dyachkov"
+__date__ ="$10.01.2012 19:43:38$"
+
+if __name__ == "__main__":
+    print ('Hello World')
